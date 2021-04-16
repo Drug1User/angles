c8bd5427af4a6e61ff9818b0d7d2aa9f77c6c6e3
KirillKN <cs-.-he-@mail.ru>
2021-04-17 00:05:05 +0500

    #3 добавил функцию для перевода из секунд в градусы, мин, секунды

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,12 @@
 function angleToSeconds(degrees, minutes, seconds) {
   return 60 * 60 * degrees + 60 * minutes + seconds;
 }
+
+
+function secondsToAngle(seconds) {
+  var degrees = parseInt(seconds / 3600);
+  var minutes = parseInt((seconds - degrees * 3600) / 60);
+  seconds = parseFloat(seconds - degrees * 3600 - minutes * 60);
+  var angle = [degrees, minutes, seconds];
+  return angle;
+}
